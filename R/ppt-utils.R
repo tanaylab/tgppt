fb7658c1992b643acb32eac3938118cdd24f85a0
Aviezer Lifshitz <aviezer.l@gmail.com>
2020-06-24 21:47:27 +0300

    added option to rasterize plotting area of ggplot object

--- R/ppt-utils.R
+++ R/ppt-utils.R
@@ -22,6 +22,7 @@ new_ppt <- function(fn) {
 #' @param inches measures are given in inches
 #' @param sep_legend plot legend and plot separatly
 #' @param transparent_bg make background of the plot transparent
+#' @param rasterize_plot rasterize the plotting panel
 #' @param new_slide add plot to a new slide
 #' @param overwrite overwrite existing file
 #'
@@ -32,7 +33,7 @@ new_ppt <- function(fn) {
 #' temp_ppt <- tempfile(fileext = ".pptx")
 #' plot_gg_ppt(gg, temp_ppt)
 #' @export
-plot_gg_ppt <- function(gg, out_ppt, height = 6, width = 6, left = 5, top = 5, inches = FALSE, sep_legend = FALSE, transparent_bg = TRUE, new_slide = FALSE, overwrite = FALSE) {
+plot_gg_ppt <- function(gg, out_ppt, height = 6, width = 6, left = 5, top = 5, inches = FALSE, sep_legend = FALSE, transparent_bg = TRUE, rasterize_plot = FALSE, new_slide = FALSE, overwrite = FALSE) {
     cm2inch <- 1
     if (!inches) {
         cm2inch <- 2.54
@@ -60,7 +61,34 @@ plot_gg_ppt <- function(gg, out_ppt, height = 6, width = 6, left = 5, top = 5, i
 
         legend <- dml(grid::grid.draw(cowplot::get_legend(gg)), bg = "transparent")
         ppt <- ppt %>% ph_with(legend, ph_location(height = height / cm2inch, width = width / cm2inch, left = left / cm2inch, top = top / cm2inch))       
+    } else if (rasterize_plot) {        
+        
+        # plot the panel and other elements separately
+        gt <- cowplot::as_gtable(p)
+        
+        # Plot the panel to png         
+        gt_panel <- gt
+        gt_panel$grobs <- gt$grobs %>% modify_at(grep("panel", gt$layout$name, invert=TRUE), ~ grid::nullGrob())
+
+        old_dev <- grDevices::dev.cur()
+        fn <- tempfile(fileext = ".png")
+        grDevices::png(fn, width = width / cm2inch, height = height / cm2inch, units="in", res=300)
+        on.exit(utils::capture.output({            
+            if (old_dev > 1) grDevices::dev.set(old_dev)
+        }))
+        grid::grid.draw(gt_panel)
+        grDevices::dev.off()
+        
+        # Plot other elements as vector graphics
+        gt_other <- gt
+        gt_other$grobs <- gt$grobs %>% modify_at(grep("panel", gt$layout$name), ~ grid::nullGrob())
+        gt_other$grobs <- gt_other$grobs %>% modify_at(grep("background", gt$layout$name), ~ grid::nullGrob())
+
+        ppt <- ppt %>% ph_with(external_img(fn), ph_location(height = height / cm2inch, width = width / cm2inch, left = left / cm2inch, top = top / cm2inch))
+        plot <- dml(grid::grid.draw(gt_other), bg = "transparent")
+        ppt <- ppt %>% ph_with(plot, ph_location(height = height / cm2inch, width = width / cm2inch, left = left / cm2inch, top = top / cm2inch))  
     } else {
+
         code <- dml(ggobj = gg, bg = "transparent")
 
         ppt <- ppt %>% ph_with(code, ph_location(height = height / cm2inch, width = width / cm2inch, left = left / cm2inch, top = top / cm2inch))
