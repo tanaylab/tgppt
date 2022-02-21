d445558ad0f718bfddbddf2b3192de87e66a1722
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2022-02-21 12:20:11 +0200

    Added control for legend position and size

--- R/ppt-utils.R
+++ R/ppt-utils.R
@@ -27,6 +27,7 @@ new_ppt <- function(fn) {
 #' @param res resolution of png used to generate rasterized plot
 #' @param new_slide add plot to a new slide
 #' @param overwrite overwrite existing file
+#' @param legend_height,legend_width,legend_left,legend_top legend position and size in case \code{sep_legend=TRUE}. Similar to to heigh,width,left and top. By default - legend would be plotted to the right of the plot.
 #'
 #' @examples
 #'
@@ -36,7 +37,7 @@ new_ppt <- function(fn) {
 #' temp_ppt <- tempfile(fileext = ".pptx")
 #' plot_gg_ppt(gg, temp_ppt)
 #' @export
-plot_gg_ppt <- function(gg, out_ppt, height = 6, width = 6, left = 5, top = 5, inches = FALSE, sep_legend = FALSE, transparent_bg = TRUE, rasterize_plot = FALSE, rasterize_legend = FALSE, res = 300, new_slide = FALSE, overwrite = FALSE) {
+plot_gg_ppt <- function(gg, out_ppt, height = 6, width = 6, left = 5, top = 5, inches = FALSE, sep_legend = FALSE, transparent_bg = TRUE, rasterize_plot = FALSE, rasterize_legend = FALSE, res = 300, new_slide = FALSE, overwrite = FALSE, legend_height = NULL, legend_width = NULL, legend_left = NULL, legend_top = NULL) {
     cm2inch <- 1
     if (!inches) {
         cm2inch <- 2.54
@@ -58,10 +59,16 @@ plot_gg_ppt <- function(gg, out_ppt, height = 6, width = 6, left = 5, top = 5, i
         )
     }
 
-    if (rasterize_plot) {
+    if (sep_legend) {
+        legend_height <- legend_height %||% height
+        legend_width <- legend_width %||% width
+        legend_top <- legend_top %||% top
+        legend_left <- legend_left %||% left + legend_width
+    }
 
-        if (sep_legend){
-            leg <- cowplot::get_legend(gg)            
+    if (rasterize_plot) {
+        if (sep_legend) {
+            leg <- cowplot::get_legend(gg)
             gg <- gg + theme(legend.position = "none")
         }
 
@@ -74,7 +81,7 @@ plot_gg_ppt <- function(gg, out_ppt, height = 6, width = 6, left = 5, top = 5, i
 
         fn <- tempfile(fileext = ".png")
         rasterize_grob(gt_panel, fn, width, height, res, cm2inch)
-        
+
         # Plot other elements as vector graphics
         gt_other <- gt
         gt_other$grobs <- gt$grobs %>% modify_at(grep("panel", gt$layout$name), ~ grid::nullGrob())
@@ -84,26 +91,25 @@ plot_gg_ppt <- function(gg, out_ppt, height = 6, width = 6, left = 5, top = 5, i
         plot <- dml(grid::grid.draw(gt_other), bg = "transparent")
         ppt <- ppt %>% ph_with(plot, ph_location(height = height / cm2inch, width = width / cm2inch, left = left / cm2inch, top = top / cm2inch))
 
-        if (sep_legend){
-            if (rasterize_legend){
+        if (sep_legend) {
+            if (rasterize_legend) {
                 fn_leg <- tempfile(fileext = ".png")
                 rasterize_grob(leg, fn_leg, width, height, res, cm2inch)
                 ppt <- ppt %>% ph_with(external_img(fn_leg), ph_location(height = height / cm2inch, width = width / cm2inch, left = left / cm2inch, top = top / cm2inch))
             } else {
                 legend <- dml(grid::grid.draw(leg), bg = "transparent")
-                ppt <- ppt %>% ph_with(legend, ph_location(height = height / cm2inch, width = width / cm2inch, left = left / cm2inch, top = top / cm2inch))
+                ppt <- ppt %>% ph_with(legend, ph_location(height = legend_height / cm2inch, width = legend_width / cm2inch, left = legend_left / cm2inch, top = legend_top / cm2inch))
             }
-            
         }
     } else {
-         if (sep_legend) {
+        if (sep_legend) {
             p <- dml(ggobj = gg + theme(legend.position = "none"), bg = "transparent")
             ppt <- ppt %>% ph_with(p, ph_location(height = height / cm2inch, width = width / cm2inch, left = left / cm2inch, top = top / cm2inch))
 
             legend <- dml(grid::grid.draw(cowplot::get_legend(gg)), bg = "transparent")
-            ppt <- ppt %>% ph_with(legend, ph_location(height = height / cm2inch, width = width / cm2inch, left = left / cm2inch, top = top / cm2inch))
+            ppt <- ppt %>% ph_with(legend, ph_location(height = legend_height / cm2inch, width = legend_width / cm2inch, left = legend_left / cm2inch, top = legend_top / cm2inch))
         } else {
-            code <- dml(ggobj = gg, bg = "transparent")                            
+            code <- dml(ggobj = gg, bg = "transparent")
             ppt <- ppt %>% ph_with(code, ph_location(height = height / cm2inch, width = width / cm2inch, left = left / cm2inch, top = top / cm2inch))
         }
     }
@@ -111,8 +117,8 @@ plot_gg_ppt <- function(gg, out_ppt, height = 6, width = 6, left = 5, top = 5, i
     print(ppt, target = out_ppt)
 }
 
-rasterize_grob <- function(grob, fn, width, height, res, cm2inch){
-    old_dev <- grDevices::dev.cur()    
+rasterize_grob <- function(grob, fn, width, height, res, cm2inch) {
+    old_dev <- grDevices::dev.cur()
     grDevices::png(fn, width = width / cm2inch, height = height / cm2inch, units = "in", res = res)
     on.exit(utils::capture.output({
         if (old_dev > 1) grDevices::dev.set(old_dev)
